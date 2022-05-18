585d3ec7504c865297312ead03df45c938ef1234
Ravi Ranjan <ravikumarranjan654@gmail.com>
2022-05-18 08:20:31 +0530

    Yaml implementation has been added

--- db/configdetails.py
+++ db/configdetails.py
@@ -1,15 +1,20 @@
 import configparser
+import yaml
 
-config=configparser.RawConfigParser()
-config.read('C://Users//Hari Priya//Desktop\pythondemo//demo-python-project//db//config.properties')
+# # config=configparser.RawConfigParser()
+# # config.read('D:\projectsss\demo-python-project\db\configuration_properties.yaml')
+
+
+with open("D:\projectsss\demo-python-project\db\configuration_properties.yaml", "r") as configfile:
+    config = yaml.safe_load(configfile)
 
 class Details:
     def __init__(self):
-        self.database_name=config.get('DB_DETAILS','DB_NAME')
-        self.database_user=config.get('DB_DETAILS','USER_NAME')
-        self.database_pwd=config.get('DB_DETAILS','PASSWORD')
-        self.database_host=config.get('DB_DETAILS','HOST')
-        self.database_port=config.get('DB_DETAILS','PORT')
+        self.database_name=config['DB_DETAILS']['DB_NAME']
+        self.database_user=config['DB_DETAILS']['USER_NAME']
+        self.database_pwd=config['DB_DETAILS']['PASSWORD']
+        self.database_host=config['DB_DETAILS']['HOST']
+        self.database_port=config['DB_DETAILS']['PORT']
 
     def get_database_name(self):
         return self.database_name
